595a6bceac7d1e813c38ab83c4715a90d4de2af4
Spicer Matthews <github@cloudmanic.com>
2018-03-01 00:20:30 -0800

    Cleaned up db models.

--- models/db.go
+++ models/db.go
@@ -7,12 +7,9 @@
 package models
 
 import (
-	"database/sql/driver"
 	"flag"
-	"fmt"
 	"go/build"
 	"os"
-	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -55,15 +52,6 @@ type Datastore interface {
 	MarkByDate(time.Time) error
 	GetMarkByDate(time.Time) (*Mark, error)
 	GetMarkAccountUnitsByAccountId(uint) float64
-}
-
-type DB struct {
-	*gorm.DB
-}
-
-// Used for date formatting with json conversion.
-type Date struct {
-	time.Time
 }
 
 //
@@ -168,49 +156,6 @@ func LoadTestingData(db *gorm.DB) {
 	db.Create(&Ledger{AccountId: 3, Date: ds, Amount: 155.45, Note: "3rd ledger test.", CategoryId: 3})
 	db.Create(&Ledger{AccountId: 4, Date: ds, Amount: 455.00, Note: "4th ledger test.", CategoryId: 1})
 
-}
-
-//
-// Convert JSON string to a date. Format XXXX-XX-XX
-//
-func (t *Date) UnmarshalJSON(b []byte) error {
-
-	// Remove quotes
-	str := strings.Replace(string(b), "\"", "", -1)
-
-	// Parse string
-	tt, _ := time.Parse("2006-01-02", str)
-
-	// Return UTC
-	*t = Date{tt.UTC()}
-	return nil
-}
-
-//
-// Convert JSON string to a date. Make it so when we create JSON we return this format XXXX-XX-XX
-//
-func (t Date) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf("\"%s\"", t.Format("2006-01-02"))), nil
-}
-
-//
-// Format time Date Going into the DB
-//
-func (t Date) Value() (driver.Value, error) {
-	return t.Format("2006-01-02"), nil
-}
-
-//
-// Convert to type Date Coming out of the DB
-//
-func (t *Date) Scan(value interface{}) error {
-
-	// Parse string
-	tt, _ := time.Parse("2006-01-02 03:04:05 -0700 MST", fmt.Sprintf("%s", value))
-
-	// Return UTC
-	*t = Date{tt.UTC()}
-	return nil
 }
 
 /* End File */
